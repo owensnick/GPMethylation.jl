eb4b2d14ba8188dd296d1ebcc98b8167336b391f
Nick Owens <owensnick@gmail.com>
2021-07-14 15:58:24 +0100

    added loadsamplemeta

--- src/loaddata.jl
+++ src/loaddata.jl
@@ -7,17 +7,34 @@
     for `n` samples and `m` probes.
 """
 function loaddata(file)
-
+    
+    
+    
 end
 
 """
     loadsamplemeta(file)
 
-    Loads sample meta - a tab or comma separated file with columns:
+    Loads sample meta - that must contain the following columns (in any order):
+
+    | Sample    | Sex   | Age   |
+    |-----------|-------|-------|
+    |  Sample_1 | F     | 1.0   |
+    |  Sample_2 | M     | 2.0   |
+    |  Sample_3 | M     | 10.0  |
+    
 
 """
-function loadsamplemeta(file)
-
+function loadsamplemeta(file, mandatorycols = [:Sample, :Sex, :Age])
+    meta = CSV.read(file, DataFrame)
+    
+    if length(intersect(mandatorycols, propertynames(meta))) != length(mandatorycols)
+        missingcols = setdist(mandatorycols, propertynames(meta))
+        colstring = join(names(meta), ", ")
+        mcolstring = join(missingcols, ", ")
+        error("Loading sample meta. Found cols: $colstring\nMissing: $mcolstring")
+    end
+    meta
 end
 
 
