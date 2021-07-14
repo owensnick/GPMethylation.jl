036fdec235758c5bd5bcf44a7a139f56d0ef4c01
Nick Owens <owensnick@gmail.com>
2021-07-14 11:02:37 +0100

    Added loaddata doc strings

---
+++ src/loaddata.jl
@@ -0,0 +1,30 @@
+
+"""
+    loaddata(file)
+
+    Loads methylation data.
+    `file` is a `RData` file containing a matrix of size `(n x m)` for `n` samples and `m` probes.
+"""
+function loaddata(file)
+
+end
+
+"""
+    loadsamplemeta(file)
+
+    Loads sample meta - a tab or comma separated file with columns:
+
+"""
+function loadsamplemeta(file)
+
+end
+
+
+"""
+    loadprobemeta(file)
+
+    Loads probe meta - a tab or comma separated file with columns:
+"""
+function loadprobemeta(file)
+
+end
