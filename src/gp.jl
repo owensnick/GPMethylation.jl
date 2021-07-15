680b56b9f75cfbd2d8c2c2c8b6fbf7e14ca28a82
Nick Owens <owensnick@gmail.com>
2021-07-15 10:40:33 +0100

    Refactored and documented gp regression functions.

--- src/gp.jl
+++ src/gp.jl
@@ -1,43 +1,82 @@
 
 
+"""
+    gp_reg_all_threads(meta, beta, gp_reg, ; nt = 16, bt = 1, n = size(R, 1), gpargs...)
 
+Wrapper to run a Gaussian Process regression function `gp_reg ∈ {gp_reg_matern52, gp_reg_const, gp_reg_linear}` with multiple threads.
+    
+Keyword argument `nt = 16` specifies number of threads. 
 
-function gp_reg_all_threads(meta, R, gp_reg, ; nt = 16, bt = 1, n = size(R, 1), gpargs...)
+Allows balance of threads to run regression on independent probes in parallel vs threads used by BLAS library for parallelising linear algebra used in regression of a single probe.
+
+Currently, recommend setting BLAS threads `bt = 1` and parallelising over multiple probes.
+
+"""
+function gp_reg_all_threads(meta, beta, gp_reg, ; nt = 16, bt = 1, n = size(R, 1), gpargs...)
         BLAS.set_num_threads(bt)
         ap = Float64.(meta.Age)
         st = range(minimum(meta.Age), maximum(meta.Age), length=50)
-        GPT = tmap(i -> gp_reg(ap, R[i, :], st, gpargs...), nt, 1:n)
+        GPT = tmap(i -> gp_reg(ap, beta[i, :], st, gpargs...), nt, 1:n)
         BLAS.set_num_threads(btc)
         GPT
 end
 
 
-function gp_reg_matern52(t, y, st, initparams=(1.0, 1.0, log(std(y))))
-    σf, ℓ, σn = initparams
-    kern = Mat52Iso(ℓ, σf)
-    gp = GP(t, y, MeanZero(), kern, σn)
+"""
+    gpreg(t, y, st, kernel, kernparams=(1.0, 1.0), logNoise=log(std(y)))
+
+Generic GP stationary zero mean regression with Gaussian likelihood. Params:
+  - `t` vector of time points (in this case ages)
+  - `y` vector of signal values (in this case prob methylation)
+  - `st` vector of time points to evaluate GP `mean` and `var`
+  - `kernel` GP `kernel` with inital params e.g. `Mat52Iso`
+  - `logσn` log(σn)
+
+Returns:
+Named tuple (gp=gp, μ=μ, v=v, st=st)
+"""
+function gpreg(t, y, st, kernel, logσn=log(std(y)))
+    gp = GP(t, y, MeanZero(), kernel, logσn)
     optimize!(gp)
     μ, v = predict_y(gp, st)
     (gp=gp, μ=μ, v=v, st=st)
 end
 
+"""
+    gpreg_matern52(t, y, st, kernparams=(1.0, 1.0) , log(std(y))))
 
-function gp_reg_const(t, y, st; initparams = (1.0, 1.0))
-    σf, σn = initparams
-    kern = Const(σf)
-    gp = GP(t, y, MeanZero(), kern, σn)
-    optimize!(gp)
-    μ, v = predict_y(gp, st)
-    (gp=gp, μ=μ, v=v, st=st)
+GP regression with `Mat52Iso(ℓ, σf)` kernel.
+`kernparms = (logℓ, log(σf))`
+`ℓ`  - initial lengthscale
+`σf` - initial function variance
+"""
+function gpreg_matern52(t, y, st, kernparams=(1.0, 1.0), logσn=log(std(y)))
+    logℓ, logσf = kernparams
+    gpreg(t, y, st, Mat52Iso(logℓ, logσf), logσn)
 end
 
+"""
+gpreg_const(t, y, st, kernparams=(1.0) , log(std(y))))
 
+GP regression with `Const(σf)`.
+`kernparms = (log(σf))`
+`σf` - initial function variance
+"""
+function gpreg_const(t, y, st, kernparams=(1.0, 1.0), logσn=log(std(y)))
+    logσf = kernparams
+    gpreg(t, y, st, Const(logσf), logσn)
+end  
 
-function gp_reg_linear(t, y, st; initparams = (1.0, 1.0, 1.0))
-    σf, ℓ, σn = initparams
-    kern = Const(σf) + LinIso(ℓ)
-    gp = GP(t, y, MeanZero(), kern, σn)
-    optimize!(gp)
-    μ, v = predict_y(gp, st)
-    (gp=gp, μ=μ, v=v, st=st)
-end
+"""
+gpreg_linear(t, y, st, kernparams=(1.0) , log(std(y))))
+
+GP regression with `Const(σf) + LinIso(ℓ)` kernel.
+`kernparms = (logℓ, log(σf))`
+`ℓ`  - initial lengthscale
+`σf` - initial function variance
+"""
+function gpreg_const(t, y, st, kernparams=(1.0, 1.0), logσn=log(std(y)))
+    logℓ, logσf = kernparams
+    gpreg(t, y, st, Const(logσf) + LinIso(logℓ), logσn)
+end  
+
