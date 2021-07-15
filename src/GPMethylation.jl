9042a884a91fd5c5fac956784ad2b6373bc7dc92
Nick Owens <owensnick@gmail.com>
2021-07-15 09:52:59 +0100

    Fixed doc strings and added R call dep

--- src/GPMethylation.jl
+++ src/GPMethylation.jl
@@ -1,11 +1,12 @@
 module GPMethylation
 
 # Write your package code here.
+using CSV: propertynames
 using DataFrames, CSV, RData
 using GaussianProcesses 
 using LinearAlgebra
 using ThreadTools
-
+using RCall
 
 export loaddata, loadsamplemeta, loadprobemeta
 
--- src/loaddata.jl
+++ src/loaddata.jl
@@ -2,9 +2,12 @@
 """
     loaddata(file)
 
-    Loads methylation data.
-    `file` is a `RData` file containing a matrix of size `(n x m)` 
-    for `n` samples and `m` probes.
+Loads methylation data.
+  
+`file` is a `RData` file containing a matrix of size `(n x m)` 
+for `n` samples and `m` probes.
+    
+
 """
 function loaddata(file)
     
@@ -15,35 +18,53 @@ end
 """
     loadsamplemeta(file)
 
-    Loads sample meta - that must contain the following columns (in any order):
+Loads sample meta, a tab or commma separated file that must contain the following columns (in any order):
 
-    | Sample    | Sex   | Age   |
-    |-----------|-------|-------|
-    |  Sample_1 | F     | 1.0   |
-    |  Sample_2 | M     | 2.0   |
-    |  Sample_3 | M     | 10.0  |
-    
+| Sample    | Sex   | Age   |
+|-----------|-------|-------|
+|  Sample_1 | F     | 1.0   |
+|  Sample_2 | M     | 2.0   |
+|  Sample_3 | M     | 10.0  |
+
+- `Sample` unique string identifier
+- `Sex`  {M, F} or missing
+- `Age` floating point age
+
+
 
 """
-function loadsamplemeta(file, mandatorycols = [:Sample, :Sex, :Age])
+loadsamplemeta(file) = loadmeta(file, [:Sample, :Sex, :Age], "sample")
+
+
+"""
+    loadprobemeta(file)
+
+Loads probe meta, a tab or commma separated file that must contain the following columns (in any order):
+
+| Probe      |
+|------------| 
+| cg00000029 |
+| cg00000103 |
+| cg00000109 |
+
+"""
+loadprobemeta(file) = loadmeta(file, [:Probe], "probe")
+    
+
+
+"""
+    loadmeta(file, mandatorycols, label)
+
+    Helper function used to load metafiles
+"""
+function loadmeta(file, mandatorycols, label)
     meta = CSV.read(file, DataFrame)
     
     if length(intersect(mandatorycols, propertynames(meta))) != length(mandatorycols)
         missingcols = setdist(mandatorycols, propertynames(meta))
         colstring = join(names(meta), ", ")
         mcolstring = join(missingcols, ", ")
-        error("Loading sample meta. Found cols: $colstring\nMissing: $mcolstring")
+        error("Loading $label meta. Found cols: $colstring\nMissing: $mcolstring")
     end
     meta
 end
-
-
-"""
-    loadprobemeta(file)
-
-    Loads probe meta - a tab or comma separated file with columns:
-"""
-function loadprobemeta(file)
-
-end
-
