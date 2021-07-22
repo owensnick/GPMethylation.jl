c0ddfa000845693459460bf3eded051b9cff88cd
Nick Owens <owensnick@gmail.com>
2021-07-22 12:12:03 +0100

    Added regression with prior export

--- src/GPMethylation.jl
+++ src/GPMethylation.jl
@@ -10,7 +10,7 @@ using RCall
 using ProgressMeter
 
 export  loaddata, loadsamplemeta, loadprobemeta, loadall, loadmeta,
-        gpreg, gpreg_const, gpreg_linear, gpreg_matern52, gpreg_all_threads, gpmodels,
+        gpreg, gpreg_const, gpreg_linear, gpreg_matern52, gpreg_all_threads, gpmodels,  gpmodels_prior, gpreg_matern52_prior, gpreg_linear_prior,
         run_gpregression, saverdata, gpstats, loadresults
        
 
