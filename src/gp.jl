09de981413397ab2e9049a8cc232cd590a5a83be
Nick Owens <owensnick@gmail.com>
2021-07-22 12:10:18 +0100

    Added regression with priors

--- src/gp.jl
+++ src/gp.jl
@@ -81,6 +81,38 @@ function gpmodels(td, yd, st)
      params, gpm.μ,  gpm.v
 end
 
+"""
+    gpmodels_prior(t, y, st)
+
+Runs GP regression for constant, linear and Matern52 kernels with priors on hyperparameters for constant and linear kernels
+"""
+function gpmodels_prior(td, yd, st)
+
+    ### for now hardcode const, linear and mat52 regression
+    ### improvement would be to specify vector of kernels and loop over
+
+    ### remove missings if necessary
+    t, y = filtmissing(td, yd)
+
+    gpc = gpreg_const(t, y, st)
+    gpl = gpreg_linear(t, y, st)
+    gpm = gpreg_matern52_prior(t, y, st)
+
+    params = (const_mll    = gpc.gp.mll,
+     const_σn    = exp(gpc.gp.logNoise.value),
+     const_σf    = sqrt(gpc.gp.kernel.σ2),
+     linear_mll  = gpl.gp.mll,
+     linear_σn   = exp(gpl.gp.logNoise.value),
+     linear_σf   = sqrt(gpl.gp.kernel.kleft.σ2),
+     linear_ℓ    = sqrt(gpl.gp.kernel.kright.ℓ2),
+     mat52_mll   = gpm.gp.mll,
+     mat52_σn    = exp(gpm.gp.logNoise.value),
+     mat52_σf    = sqrt(gpm.gp.kernel.σ2),
+     mat52_ℓ     = gpm.gp.kernel.ℓ)
+
+     params, gpm.μ,  gpm.v
+end
+
 
 """
     gpreg(t, y, st, kernel, kernparams=(1.0, 1.0), logNoise=log(std(y)))
@@ -100,6 +132,24 @@ function gpreg(t, y, st, kernel, logσn=log(std(y)))
     optimize!(gp)
     μ, v = predict_y(gp, st)
     (gp=gp, μ=μ, v=v)
+end
+
+"""
+    gpreg_matern52(t, y, st, kernparams=(1.0, log(mean(y))) , log(std(y))))
+
+GP regression with `Mat52Iso(ℓ, σf)` kernel.
+
+`kernparms = (logℓ, log(σf))`
+
+`ℓ`  - initial lengthscale
+
+`σf` - initial function variance
+"""
+function gpreg_matern52_prior(t, y, st, kernparams=(2.0, log(mean(y))), logσn=log(std(y)), kernpriors=[Normal(4.7, 1.2), Normal(-1, 6)])
+    logℓ, logσf = kernparams
+    kern = Mat52Iso(logℓ, logσf)
+    set_priors!(kern, kernpriors)
+    gpreg(t, y, st, kern, logσn)
 end
 
 """
@@ -148,3 +198,22 @@ function gpreg_linear(t, y, st, kernparams=(1.0, log(mean(y))), logσn=log(std(y
     gpreg(t, y, st, Const(logσf) + LinIso(logℓ), logσn)
 end  
 
+"""
+    gpreg_linear_prior(t, y, st, kernparams=(1.0) , log(std(y))))
+
+GP regression with `Const(σf) + LinIso(ℓ)` kernel.
+
+`kernparms = (logℓ, log(σf))`
+
+`ℓ`  - initial lengthscale
+
+`σf` - initial function variance
+"""
+function gpreg_linear_prior(t, y, st, kernparams=(1.0, log(mean(y))), logσn=log(std(y)), kernpriors=[Normal(5.7, 2.2)])
+    logℓ, logσf = kernparams
+    kc = Const(logσf)
+    kl = LinIso(logℓ)
+    set_priors!(kl, kernpriors)
+    k = kc + kl
+    gpreg(t, y, st, k, logσn)
+end  
